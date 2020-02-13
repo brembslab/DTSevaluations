9e769e34e0e6b750fea3f6768488be7d9fc1249e
brembs <bjoern@brembs.net>
2020-02-13 19:04:01 +0100

    small fixes

--- readXMLdatafile.R
+++ readXMLdatafile.R
@@ -156,7 +156,7 @@ for (l in 1:length(xml_list))
   metadata <- data.frame(lapply(metadata, as.character), stringsAsFactors=FALSE) #convert factors to characters
   colnames(metadata)=xml_list #add filenames to metadata dataframe
   offending_metanames <- colnames(metadata[metadata %in% unique(as.list(metadata))[-1]]) #store offending filenames for each deviation
-#  if(!is_empty(offending_metanames)){return(offending_metanames)}else{return(NULL)} #return vector with offending filenames or NULL if empty
+  if(!is_empty(offending_metanames)){return(offending_metanames)}else{return(NULL)} #return vector with offending filenames or NULL if empty
 }
 
 #### make sure there are no duplicated traces in the list ####
@@ -167,14 +167,14 @@ MultiFlyDuplicateCheck <- function(xml_list)
     xml_name=xml_list[l]
     ## read data and extract traces
     singledata <- flyDataImport(xml_name)
-    behav = singledata[[13]]$fly
+    temp.behav = singledata[[13]]$fly
     if(l==1){
-      behavior<-data.frame(behav)} else {
-        behavior[,l]<-data.frame(behav)}
+      behavior<-data.frame(temp.behav)} else {
+        behavior[,l]<-data.frame(temp.behav)}
   }
   colnames(behavior)=xml_list #add filenames to behavior dataframe
-  offending_behavnames = names(which(duplicated(t(behavior))))
-#  if(!is_empty(offending_behavnames)){return(offending_behavnames)}else{return(NULL)} #return vector with offending filenames or NULL if empty
+  offending_behavnames = colnames(behavior[behavior %in% behavior[which(duplicated(t(behavior)))]]) #find the pairs of files which are duplicated
+  if(!is_empty(offending_behavnames)){return(offending_behavnames)}else{return(NULL)} #return vector with offending filenames or NULL if empty
 }
 
 ##gather experimental metadata in a single vector for plotting in summary pages
