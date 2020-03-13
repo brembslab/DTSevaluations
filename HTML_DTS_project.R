7342c6eae1d6fbc82721c1b521ba0bd25283f8a2
brembs <bjoern@brembs.net>
2020-03-13 16:38:36 +0100

    pooling goups by description
    should work, only cosmetic improvement left

--- HTML_DTS_project.R
+++ HTML_DTS_project.R
@@ -415,6 +415,10 @@ if(NofGroups>2 & length(unique(groupdescriptions))==2){
   PIprofilePooled=dcast(PIstatPooled[c("group", "PIs")], PIs~...)[,2:3] #create new dataframe for PIs in the pooled groups this doesn't work with unequal samplesizes: as.data.frame(lapply(dcast(PIstatPooled[c("group", "PIs")], PIs~...), na.omit))
   samplesizesPooled=colSums(!is.na(PIprofilePooled))  #find the new samplesizes for the different groups
   
+  #create table with pooled groups for later plotting
+  sq <- seq(max(length(pool1), length(pool2)))
+  PooledGroups <- data.frame(pool1[sq], pool2[sq])
+  colnames(PooledGroups)=unique(groupdescriptions)
 }
 
 ###### continue for all projects with two groups
