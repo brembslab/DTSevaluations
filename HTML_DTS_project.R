f0e7dd545c8f7358f332ad6f71b436cfbf0e6f1a
Anders Eriksson <Anders.Eriksson@ur.de>
2020-04-01 12:15:37 +0200

    Revert "Update HTML_DTS_project.R"
    This reverts commit 316e2a27f7655265bb9c529cd372ede24f7f50de.

--- HTML_DTS_project.R
+++ HTML_DTS_project.R
@@ -39,9 +39,8 @@ project.file <- file.choose()
 project.path = dirname(project.file)
 project.data<-yaml.load_file(project.file)
 
-#measure the runtime 
+#measure the runtime of the whole analysis 
 start_time <- Sys.time() #Records the system time at the start of the analysis
-totalflies <- length(paste(project.path, unlist(do.call("rbind", lapply(project.data$resources, '[', 4))), sep = "/")) #saves the total number of flies
 
 #make sure all flies have the identical experimental design and find out which don't
 xml_list = paste(project.path, unlist(do.call("rbind", lapply(project.data$resources, '[', 4))), sep = "/") #create list of all file names
@@ -409,7 +408,7 @@ rmarkdown::render(paste(start.wd,"/project.Rmd", sep=""),
                   output_file = paste(project.data$experiment$name,"html", sep = "."), 
                   output_dir = evaluation.path)
 #### end RMarkdown for project evaluations #################################################
-
-Runtime = round(((Sys.time() - start_time)), 3) #Subtracts the endtime with the starttime to get the total analysis time
-print(paste("Runtime per fly was ", ((Runtime)*60)/totalflies, " seconds", ", in total ", round(Runtime, 3), " minutes")) #prints the time per fly and the total time
+end_time <- Sys.time() #records the end of the analysis
+Runtime = round(((end_time - start_time)/length(xml_list)), 3) #Subtracts the endtime with the starttime to get the total analysis time
+print(paste0("Runtime per fly was ", ((Runtime)*60)/length(xml_list), " seconds", ", in total ", round(Runtime, 3), " minutes")) #prints the time per fly and the total time
 setwd(start.wd)
