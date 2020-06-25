bbc7badbf9783271e757076014475c8b735b41cb
brembs <bjoern@brembs.net>
2020-06-25 16:59:37 +0200

    updated single file evaluations

--- single_file_HTML.R
+++ single_file_HTML.R
@@ -20,6 +20,14 @@ library(rmarkdown)
 library(markdown)
 library(knitr)
 library(zoo)
+library(tidyverse)
+library(questionr)
+library(data.table)
+library(DescTools)
+library(magick)
+library(reactable)
+
+
 ## source the script with the functions needed for analysis
 source("readXMLdatafile.R")
 source("DTS_plotfunctions.R")
@@ -40,6 +48,8 @@ period.data <- list()     #data grouped by period
 grouped.data <- list()    #total data grouped
 speclist <- list()        #spectograms
 l=1                       #set fly counter to 1
+#create dataframes for dwelling data
+dwelldata = dwellplots = grouped.dwell = list()
 
 
 #start evaluating
@@ -73,7 +83,16 @@ l=1                       #set fly counter to 1
     #create/empty plot lists
     poshistos <- list()
     flyhistos <- list()
+
+    Dwell = any(c("yt","color","sw","fs") %in% sequence$type) ###determine if dwelling times should be calculated
+    if (Dwell & l==1){
+        dwellmeans = list()
+        dwellmeans$unpunished <- dwellmeans$punished <- data.frame(matrix(ncol = NofPeriods))
+    }
+
+    nonOMperiods=which(!grepl("optomotor", sequence$type)==TRUE) #vector containing period numbers for non-optomotor periods
     
+        
 #what kind of experiment are we dealing with? Default is torquemeter
 if (tolower(ExpType=="torquemeter")){FlyBehavior="Torque"} else {FlyBehavior="Platform Position"}
     
