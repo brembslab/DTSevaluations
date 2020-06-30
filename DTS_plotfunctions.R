34176c9df6ab3a50dcd69433442acd8a850fc0f5
Anders Eriksson <Anders.Eriksson@ur.de>
2020-06-30 15:36:36 +0200

    changed rectangle function

--- DTS_plotfunctions.R
+++ DTS_plotfunctions.R
@@ -15,9 +15,9 @@ fly_pos_traces <- function(temp)
   #shade quadrants
   rect(temp$time[1],-1350,temp$time[nrow(temp)],-450, col = "grey95")
   rect(temp$time[1],450,temp$time[nrow(temp)],1350, col = "grey95")
-    par(new=TRUE)
+  par(new=TRUE)
   plot(x = temp$time, y = pos, type = "l", col="red3", xaxs = "i", yaxs = "i", ylim=c(-1800,1800), ylab = "Position [arb.units]",xlab="time [ms]")
-    par(new=TRUE)
+  par(new=TRUE)
   plot(x = temp$time, temp$fly, type = "l", col="blue", xaxs = "i", ylim=maxfly, main = paste("Fly Traces", flyname, "Period", i), axes=F, xlab=NA, ylab="")
   lines(c(temp$time[1],temp$time[nrow(temp)]),c(0,0),type="l",lty=1,lwd=1, col="black")
   axis(4)
@@ -37,7 +37,7 @@ dytraces <- function(rawdata)
     dyAxis("y2", label = paste(FlyBehavior, "[arb units]"), independentTicks = TRUE, valueRange = maxfly, drawGrid = FALSE) %>%
     dyOptions(includeZero = TRUE) %>%
     dyRangeSelector()
-
+  
   return(traces)
 }
 
@@ -47,9 +47,9 @@ dytraces <- function(rawdata)
 wilcox.annotate <- function(boxes, wilcoxon)
 {
   annotate("text",
-              x=boxes,
-              y=+Inf, vjust = 1.5,
-              label=paste("p=",wilcoxon[boxes]))
+           x=boxes,
+           y=+Inf, vjust = 1.5,
+           label=paste("p=",wilcoxon[boxes]))
 }
 
 #samplesizes annotations
@@ -160,7 +160,7 @@ plotrawOMtraces <- function(OMdata, omtitle){
 #########plot averaged optomotor traces of a single fly##################
 plotaveOMtraces <- function(OMdata){
   OMtraces <- ggplot(data=OMdata, aes(x=OMdata$time/1000, y=OMdata[[as.character(flyname)]])) + 
-    geom_rect(aes(xmin = mean(OMdata$time/1000),xmax = Inf,ymin = -Inf, ymax = Inf),fill=("grey"), alpha = 0.01)+
+    annotate("rect", xmin = mean(OMdata$time/1000),xmax = Inf,ymin = -Inf, ymax = Inf,alpha = .2)+
     geom_hline(yintercept = 0) +
     geom_line() +
     ylab("Optomotor Resoponse [rel. units]")+ 
@@ -175,42 +175,42 @@ plotaveOMtraces <- function(OMdata){
 
 #########plot averaged optomotor traces of several flies #################
 plotOMtracesMean <- function(OMdata){
-    plotOM=ldply(OMdata, data.frame)            #move the dataframes for each group into a single dataframe
-    plotOM=plotOM[,c("time","means","sd","group")]
-    
-    #plot averaged OM traces
-    
-    meanOMtraces <- ggplot(plotOM, aes(x=time/1000, y=means, group = group)) +
-            theme(panel.grid.major.x = element_blank(),panel.grid.major.y = element_line( size=.1, color="grey"))+
-            geom_rect(aes(xmin = mean(plotOM$time/1000),xmax = Inf ,ymin = -Inf, ymax = Inf),fill=("grey"), alpha = 0.01)+
-            geom_hline(yintercept = 0, color="black") +
-            geom_ribbon(aes(ymin=means-sd, ymax=means+sd, fill = group), alpha=0.5) +
-            scale_fill_manual(values = boxcolors) +
-            geom_line(aes(colour = group), size = 1) + 
-            scale_color_manual(values = boxcolors) +
-            ggtitle("Mean Optomotor Traces and Standard Deviations") +
-            guides(colour = guide_legend(override.aes = list(size=3))) +
-            theme_light(base_size = 16) + 
-            theme(legend.justification=c(1,0),
-                  legend.position="right", 
-                  legend.title=element_blank(), 
-                  legend.key.size = unit(2, 'lines'),
-                  legend.key = element_rect(size = 6),
-                  legend.box.background = element_rect(fill="white"),
-                  legend.box.margin = margin(4, 4, 4, 4),
-                  legend.text=element_text(size=14), 
-                  panel.grid.major.y = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.border = element_rect(size = 0.5, linetype = "solid", colour = "black", fill=NA)) +
-            theme(axis.text.y = element_text(size=12))+
-            ylab("Optomotor Response [rel. units]") + 
-            xlab("Time [s]")+
-            annotate("text", -Inf, -Inf, label = "Right (clockwise) arena rotations", hjust = -.4, vjust = -1.3)+
-            annotate("text", Inf, Inf, label = "Left (counter-clockwise) arena rotations", hjust =1.1, vjust = 1.5)+ 
-            theme(panel.grid.major.x = element_blank(),panel.grid.major.y = element_line( size=.1, color="grey"))+
-            scale_x_continuous(expand = expand_scale(add = 0))
-    return(meanOMtraces)
-
+  plotOM=ldply(OMdata, data.frame)            #move the dataframes for each group into a single dataframe
+  plotOM=plotOM[,c("time","means","sd","group")]
+  
+  #plot averaged OM traces
+  
+  meanOMtraces <- ggplot(plotOM, aes(x=time/1000, y=means, group = group)) +
+    theme(panel.grid.major.x = element_blank(),panel.grid.major.y = element_line( size=.1, color="grey"))+
+    annotate("rect", xmin = mean(plotOM$time/1000),xmax = Inf,ymin = -Inf, ymax = Inf,alpha = .2)+
+  geom_hline(yintercept = 0, color="black") +
+    geom_ribbon(aes(ymin=means-sd, ymax=means+sd, fill = group), alpha=0.5) +
+    scale_fill_manual(values = boxcolors) +
+    geom_line(aes(colour = group), size = 1) + 
+    scale_color_manual(values = boxcolors) +
+    ggtitle("Mean Optomotor Traces and Standard Deviations") +
+    guides(colour = guide_legend(override.aes = list(size=3))) +
+    theme_light(base_size = 16) + 
+    theme(legend.justification=c(1,0),
+          legend.position="right", 
+          legend.title=element_blank(), 
+          legend.key.size = unit(2, 'lines'),
+          legend.key = element_rect(size = 6),
+          legend.box.background = element_rect(fill="white"),
+          legend.box.margin = margin(4, 4, 4, 4),
+          legend.text=element_text(size=14), 
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(size = 0.5, linetype = "solid", colour = "black", fill=NA)) +
+    theme(axis.text.y = element_text(size=12))+
+    ylab("Optomotor Response [rel. units]") + 
+    xlab("Time [s]")+
+    annotate("text", -Inf, -Inf, label = "Right (clockwise) arena rotations", hjust = -.4, vjust = -1.3)+
+    annotate("text", Inf, Inf, label = "Left (counter-clockwise) arena rotations", hjust =1.1, vjust = 1.5)+ 
+    theme(panel.grid.major.x = element_blank(),panel.grid.major.y = element_line( size=.1, color="grey"))+
+    scale_x_continuous(expand = expand_scale(add = 0))
+  return(meanOMtraces)
+  
 }
 
 
@@ -277,98 +277,98 @@ plotOMParamBox <- function(v, plotOMparams, samplesizes, OMvariables, OMtitles){
 # Find the midpoint in left and right turning in averaged traces (OMdata)
 OMparamextract <- function(OMdata){
   
-OMmidpoint = OMdata$time[nrow(OMdata)]/2
-
-#right turning traces
-x1=OMdata$time[OMdata$time<=OMmidpoint]                     #time
-y=OMdata[[as.character(flyname)]][OMdata$time<=OMmidpoint]  #mean right OM response
-y=rollapply(y, 100,mean, fill="extend")                     #apply rolling average of 100 data points (5s)
-xy <-sortedXyData(x1,y)                                     #create sortedXYdata object
-
-LinMod = lm(y ~ x1, data = xy)                              #fit linear model to OMdata right turning
-
-#uncomment for debugging
-#plot(xy)
-#abline(LinMod)
-#LinMod$coefficients[2]
-
-if(abs(LinMod$coefficients[2])<0.0001){                     #if the linear model shows hardly any slope, set asymptote as mean of all values
-  right=c(b0=0, b1=mean(y), lrc=-1000)
-} else {
-  right<-tryCatch(                                          #extract the right turning coefficients into a vector. Set vector to zero for errors
-    expr = {
-      right<-NLSstAsymptotic(xy)
-    },
-    error = function(e){
-      right=c(b0=0, b1=0, lrc=-1000)                        #set logarithmic rate constant to -1000 so exp(lrc)=0
-      return(right)
-    },
-    warning = function(w){
-      right=c(b0=0, b1=0, lrc=-1000)                        #set logarithmic rate constant to -1000 so exp(lrc)=0
-      return(right)
-    },
-    finally = {}
-  )}
-
-#left turning traces
-x1=OMdata$time[OMdata$time>OMmidpoint]                      #time
-x1=x1-x1[1]                                                 #set time to start from zero
-y=OMdata[[as.character(flyname)]][OMdata$time>OMmidpoint]   #mean left OM response
-y=rollapply(y, 100,mean, fill="extend")                     #apply rolling average of 100 data points (5s)
-xy <-sortedXyData(x1,y)                                     #create sortedXYdata object
-
-LinMod = lm(y ~ x1, data = xy)                              #fit linear model to OMdata left turning
-
-#uncomment for debugging
-#plot(xy)
-#abline(LinMod)
-#LinMod$coefficients[2]
-
-if(abs(LinMod$coefficients[2])<0.0001){                     #if the linear model shows hardly any slope, set asymptote as mean of all values
-  left=c(b0=0, b1=mean(y), lrc=-1000)
-} else {
-  left<-tryCatch(                                           #extract the left turning coefficients into a vector. Set vector to zero for errors
-    expr = {
-      left<-NLSstAsymptotic(xy)
-    },
-    error = function(e){
-      left=c(b0=0, b1=0, lrc=-1000)                         #set logarithmic rate constant to -1000 so exp(lrc)=0
-      return(left)
-    },
-    warning = function(w){
-      left=c(b0=0, b1=0, lrc=-1000)                         #set logarithmic rate constant to -1000 so exp(lrc)=0
-      return(left)
-    },
-    finally = {}
-  )}
-
-#uncomment for debugging
-#print("right: ")
-#print(right[1])
-#print(right[2])
-#print("<br>left: ")
-#print(left[1])
-#print(left[2])
-
-###calculate averages and asymmetries to three significant digits and put them into a vector
-rightasymp = right[1]+right[2]
-leftasymp = left[1]+left[2]
-#sanity checks
-if(!between(leftasymp, maxfly[1], maxfly[2])){leftasymp=0}
-if(!between(rightasymp, maxfly[1], maxfly[2])){rightasymp=0}
-
-if (leftasymp==0 & rightasymp==0){aveOMcoeff=c(0,0,0)
-} else {
-  aveOMcoeff=c(signif((abs(rightasymp)+abs(leftasymp))/2, 3),                                #mean asymptote
-               signif(((exp(right[3])+exp(left[3]))/2)*1000, 3),                             #mean rate constant [s]
-               signif((abs(rightasymp)-abs(leftasymp))/(abs(rightasymp)+abs(leftasymp)), 3)) #asymmetry index
-}
-
-###make vector into a dataframe for easy exporting and plotting
-tempOMparams <- as.data.frame(rbind(as.numeric(aveOMcoeff)))      #convert to dataframe
-names(tempOMparams) = c("asymptote", "rate constant","AI")        #set column names
-rownames(tempOMparams)[1]=as.character(flyname)                   #set row names to flynames
-
-return(tempOMparams)
+  OMmidpoint = OMdata$time[nrow(OMdata)]/2
+  
+  #right turning traces
+  x1=OMdata$time[OMdata$time<=OMmidpoint]                     #time
+  y=OMdata[[as.character(flyname)]][OMdata$time<=OMmidpoint]  #mean right OM response
+  y=rollapply(y, 100,mean, fill="extend")                     #apply rolling average of 100 data points (5s)
+  xy <-sortedXyData(x1,y)                                     #create sortedXYdata object
+  
+  LinMod = lm(y ~ x1, data = xy)                              #fit linear model to OMdata right turning
+  
+  #uncomment for debugging
+  #plot(xy)
+  #abline(LinMod)
+  #LinMod$coefficients[2]
+  
+  if(abs(LinMod$coefficients[2])<0.0001){                     #if the linear model shows hardly any slope, set asymptote as mean of all values
+    right=c(b0=0, b1=mean(y), lrc=-1000)
+  } else {
+    right<-tryCatch(                                          #extract the right turning coefficients into a vector. Set vector to zero for errors
+      expr = {
+        right<-NLSstAsymptotic(xy)
+      },
+      error = function(e){
+        right=c(b0=0, b1=0, lrc=-1000)                        #set logarithmic rate constant to -1000 so exp(lrc)=0
+        return(right)
+      },
+      warning = function(w){
+        right=c(b0=0, b1=0, lrc=-1000)                        #set logarithmic rate constant to -1000 so exp(lrc)=0
+        return(right)
+      },
+      finally = {}
+    )}
+  
+  #left turning traces
+  x1=OMdata$time[OMdata$time>OMmidpoint]                      #time
+  x1=x1-x1[1]                                                 #set time to start from zero
+  y=OMdata[[as.character(flyname)]][OMdata$time>OMmidpoint]   #mean left OM response
+  y=rollapply(y, 100,mean, fill="extend")                     #apply rolling average of 100 data points (5s)
+  xy <-sortedXyData(x1,y)                                     #create sortedXYdata object
+  
+  LinMod = lm(y ~ x1, data = xy)                              #fit linear model to OMdata left turning
+  
+  #uncomment for debugging
+  #plot(xy)
+  #abline(LinMod)
+  #LinMod$coefficients[2]
+  
+  if(abs(LinMod$coefficients[2])<0.0001){                     #if the linear model shows hardly any slope, set asymptote as mean of all values
+    left=c(b0=0, b1=mean(y), lrc=-1000)
+  } else {
+    left<-tryCatch(                                           #extract the left turning coefficients into a vector. Set vector to zero for errors
+      expr = {
+        left<-NLSstAsymptotic(xy)
+      },
+      error = function(e){
+        left=c(b0=0, b1=0, lrc=-1000)                         #set logarithmic rate constant to -1000 so exp(lrc)=0
+        return(left)
+      },
+      warning = function(w){
+        left=c(b0=0, b1=0, lrc=-1000)                         #set logarithmic rate constant to -1000 so exp(lrc)=0
+        return(left)
+      },
+      finally = {}
+    )}
+  
+  #uncomment for debugging
+  #print("right: ")
+  #print(right[1])
+  #print(right[2])
+  #print("<br>left: ")
+  #print(left[1])
+  #print(left[2])
+  
+  ###calculate averages and asymmetries to three significant digits and put them into a vector
+  rightasymp = right[1]+right[2]
+  leftasymp = left[1]+left[2]
+  #sanity checks
+  if(!between(leftasymp, maxfly[1], maxfly[2])){leftasymp=0}
+  if(!between(rightasymp, maxfly[1], maxfly[2])){rightasymp=0}
+  
+  if (leftasymp==0 & rightasymp==0){aveOMcoeff=c(0,0,0)
+  } else {
+    aveOMcoeff=c(signif((abs(rightasymp)+abs(leftasymp))/2, 3),                                #mean asymptote
+                 signif(((exp(right[3])+exp(left[3]))/2)*1000, 3),                             #mean rate constant [s]
+                 signif((abs(rightasymp)-abs(leftasymp))/(abs(rightasymp)+abs(leftasymp)), 3)) #asymmetry index
+  }
+  
+  ###make vector into a dataframe for easy exporting and plotting
+  tempOMparams <- as.data.frame(rbind(as.numeric(aveOMcoeff)))      #convert to dataframe
+  names(tempOMparams) = c("asymptote", "rate constant","AI")        #set column names
+  rownames(tempOMparams)[1]=as.character(flyname)                   #set row names to flynames
+  
+  return(tempOMparams)
 }
 
