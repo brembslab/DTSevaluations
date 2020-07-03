c90732222fb72833ae7a1b0b980ac8398e90615d
brembs <bjoern@brembs.net>
2020-07-03 16:12:36 +0200

    fixed identical() code

--- HTML_DTS_project.R
+++ HTML_DTS_project.R
@@ -69,20 +69,20 @@ offending_behavnames <- MultiFlyDuplicateCheck(xml_list)
 if(!is.null(offending_behavnames)) stop("There are duplicates in the raw data!", cat("Error! List of duplicate file(s): ", offending_behavnames, sep = "\n"))
 
 #collecting essential data from the project file for statistics and plots
-NofGroups = unname(lengths(project.data["resources"]))                                         #get number of experimental groups
-samplesizes = unname(lengths(sapply(project.data[["resources"]], function(x) x["data"])))      #get samplesizes
-groupnames <- unlist(sapply(project.data[["resources"]], function(x) x["name"]))               #get a vector with all group names
-names(samplesizes) = groupnames                                                                #name the samplesizes to assign them to groups
-sortedSsizes = sort(samplesizes, decreasing = TRUE)                                            #samplesizes sorted in ascending alphabetical groupname order
-groupdescriptions <- unlist(sapply(project.data[["resources"]], function(x) x["description"])) #get a vector with all group descriptions
-signif = project.data[["statistics"]][["significance-levels"]]                                 #get significance levels
-priorval = project.data[["statistics"]][["priors"]]                                            #get priors for FPR calculation
-twogroupstats <- identical(1,project.data[["statistics"]][["two.groups"]][["data"]])           #determine if statistics for two groups are required
-threegroupstats <- identical(1,project.data[["statistics"]][["three.groups"]][["data"]])       #determine if statistics for three groups are required
-wil <- identical(1,project.data[["statistics"]][["single.groups"]][["data"]])                  #determine if we need to do single tests
-learningscore = project.data[["statistics"]][["learning-score"]][["data"]]                     #get the PI that is going to be tested
+NofGroups = unname(lengths(project.data["resources"]))                                               #get number of experimental groups
+samplesizes = unname(lengths(sapply(project.data[["resources"]], function(x) x["data"])))            #get samplesizes
+groupnames <- unlist(sapply(project.data[["resources"]], function(x) x["name"]))                     #get a vector with all group names
+names(samplesizes) = groupnames                                                                      #name the samplesizes to assign them to groups
+sortedSsizes = sort(samplesizes, decreasing = TRUE)                                                  #samplesizes sorted in ascending alphabetical groupname order
+groupdescriptions <- unlist(sapply(project.data[["resources"]], function(x) x["description"]))       #get a vector with all group descriptions
+signif = project.data[["statistics"]][["significance-levels"]]                                       #get significance levels
+priorval = project.data[["statistics"]][["priors"]]                                                  #get priors for FPR calculation
+twogroupstats <- identical(1,as.numeric(project.data[["statistics"]][["two.groups"]][["data"]]))     #determine if statistics for two groups are required
+threegroupstats <- identical(1,as.numeric(project.data[["statistics"]][["three.groups"]][["data"]])) #determine if statistics for three groups are required
+wil <- identical(1,as.numeric(project.data[["statistics"]][["single.groups"]][["data"]]))            #determine if we need to do single tests
+learningscore = project.data[["statistics"]][["learning-score"]][["data"]]                           #get the PI that is going to be tested
 if(!is.null(unlist(sapply(project.data[["resources"]], function(x) x["id"])))){
-groupids <- hyperlinks.FBids(unlist(sapply(project.data[["resources"]], function(x) x["id"]))) #get a vector with all group FlyBase IDs and hyperlinks
+groupids <- hyperlinks.FBids(unlist(sapply(project.data[["resources"]], function(x) x["id"])))       #get a vector with all group FlyBase IDs and hyperlinks
 } else groupids=NULL
 
 #what kind of experiment are we dealing with? Default is torquemeter
