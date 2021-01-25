5cf2fbb0d64427e14fb43b7b5ac9c686d447ee74
Björn Brembs <bjoern@brembs.net>
2021-01-25 19:51:54 +0100

    raincloudplots implemented and tested once

--- include/extractlearningscores.R
+++ include/extractlearningscores.R
@@ -1,4 +1,4 @@
-### DTS script to extract the PI learningscore values for the period specified in the project yaml file
+### DTS script to extract the PI learningscore values for the period specified in the project yaml file (and some other PIs)
 if(PIs_present & !is.null(learningscore)){
   PIstat <- list()
   CatStat <- list()
@@ -22,16 +22,15 @@ if(PIs_present & !is.null(learningscore)){
 }
 
 #Compute average pretest values and create dataframe for 2x2 repeated measures raincloudplot
-if(PIs_present & twogroupstats){
+if(PIs_present & twogroupstats &NofGroups==2){
   pretestPIs <- list()
   for(x in 1:NofGroups){
-    pretestPIs[[x]] <- grouped.PIprofiles[[x]][pretestperiods]     #extract the PIs into a temproary list
+    pretestPIs[[x]] <- grouped.PIprofiles[[x]][pretestperiods]     #extract the PIs into a temporary list
     pretestPIs[[x]] <- rowMeans(pretestPIs[[x]])
   }
   pretestPIs <- as.data.frame(t(plyr::ldply(pretestPIs, rbind)))                    #convert PI list to a temproary data.frame
   colnames(pretestPIs) <- unlist(sapply(project.data[["resources"]], '[', 'name'))  #add group names as column names to PItemp
   pretestPIs <- melt(pretestPIs, measure.vars = names(pretestPIs), variable.name = "group", value.name = "category") #melt categories into dataframe with group as id-variable
-}
 
 #Create the raincloudplot dataframe
 rcp.PIs <- data_2x2(
@@ -39,23 +38,8 @@ rcp.PIs <- data_2x2(
   array_2 = pretestPIs$category[pretestPIs$group==groupnames[2]],
   array_3 = PIstat.rcp$PIs[PIstat.rcp$group==groupnames[1]],
   array_4 = PIstat.rcp$PIs[PIstat.rcp$group==groupnames[2]],
-  labels = (c('congruent','incongruent')),
+  labels = (groupnames),
   jit_distance = .09,
   jit_seed = 321,
   spread_x_ticks = FALSE)
-
-
-raincloud_2x2 <- raincloud_2x2_repmes(
-  data = na.omit(rcp.PIs),
-  colors = (c('dodgerblue', 'darkorange', 'dodgerblue', 'darkorange')),
-  fills = (c('dodgerblue', 'darkorange', 'dodgerblue', 'darkorange')),
-  size = 1,
-  alpha = .6,
-  spread_x_ticks = FALSE) +
-  
-  scale_x_continuous(breaks=c(1,2), labels=c("Pre", "Post"), limits=c(0, 3)) +
-  xlab("Time") + 
-  ylab("Score") +
-  theme_classic()
-
-raincloud_2x2
+}
