ed3d2e6836e1f975e2d6f27691914b0d7eb486ea
brembs <bjoern@brembs.net>
2024-02-20 10:47:30 +0100

    added SEM option

--- include/optomotor.R
+++ include/optomotor.R
@@ -7,6 +7,7 @@ if(any(grepl("optomotor", sequence$type)==TRUE)){    ###determine if there are o
       #before
       OMdataBefore$means=rowMeans(OMdataBefore[-1])
       OMdataBefore$sd=rowSds(OMdataBefore[-1])
+      OMdataBefore$sem=rowSEMs(OMdataBefore[-1])
       OMdataBefore$group=dataset.data[["resources"]][[x]][["name"]]
       grouped.OMdataBefore[[x]] <- OMdataBefore #save optomotor data to groupwise list
       rm(OMdataBefore) #remove the optomotor data frame so it can be generated again for the next group
@@ -17,6 +18,7 @@ if(any(grepl("optomotor", sequence$type)==TRUE)){    ###determine if there are o
       #after
       OMdataAfter$means=rowMeans(OMdataAfter[-1])
       OMdataAfter$sd=rowSds(OMdataAfter[-1])
+      OMdataAfter$sem=rowSEMs(OMdataAfter[-1])
       OMdataAfter$group=dataset.data[["resources"]][[x]][["name"]]
       OMparamsAfter$desc=dataset.data[["resources"]][[x]][["description"]]
       grouped.OMdataAfter[[x]] <- OMdataAfter #save optomotor data to groupwise list
@@ -27,12 +29,14 @@ if(any(grepl("optomotor", sequence$type)==TRUE)){    ###determine if there are o
       #before_swapped
       OMdataBefore_swapped$means=rowMeans(OMdataBefore_swapped[-1])
       OMdataBefore_swapped$sd=rowSds(OMdataBefore_swapped[-1])
+      OMdataBefore_swapped$sem=rowSEMs(OMdataBefore_swapped[-1])
       OMdataBefore_swapped$group=dataset.data[["resources"]][[x]][["name"]]
       grouped.OMdataBefore_swapped[[x]] <- OMdataBefore_swapped #save optomotor data to groupwise list
       rm(OMdataBefore_swapped) #remove the optomotor data frame so it can be generated again for the next group
       #after_swapped
       OMdataAfter_swapped$means=rowMeans(OMdataAfter_swapped[-1])
       OMdataAfter_swapped$sd=rowSds(OMdataAfter_swapped[-1])
+      OMdataAfter_swapped$sem=rowSEMs(OMdataAfter_swapped[-1])
       OMdataAfter_swapped$group=dataset.data_swapped[["resources"]][[x]][["name"]]
       OMparamsAfter_swapped$desc=dataset.data_swapped[["resources"]][[x]][["description"]]
       grouped.OMdataAfter_swapped[[x]] <- OMdataAfter_swapped #save optomotor data to groupwise list
@@ -41,6 +45,7 @@ if(any(grepl("optomotor", sequence$type)==TRUE)){    ###determine if there are o
   } else {
     OMdata$means=rowMeans(OMdata[-1])
     OMdata$sd=rowSds(OMdata[-1])
+    OMdata$sem=rowSEMs(OMdata[-1])
     OMdata$group=dataset.data[["resources"]][[x]][["name"]]
     grouped.OMdata[[x]] <- OMdata #save optomotor data to groupwise list
     rm(OMdata) #remove the optomotor data frame so it can be generated again for the next group
@@ -51,6 +56,7 @@ if(any(grepl("optomotor", sequence$type)==TRUE)){    ###determine if there are o
     #swapped for plotting
     OMdata_swapped$means=rowMeans(OMdata_swapped[-1])
     OMdata_swapped$sd=rowSds(OMdata_swapped[-1])
+    OMdata_swapped$sem=rowSEMs(OMdata_swapped[-1])
     OMdata_swapped$group=dataset.data[["resources"]][[x]][["name"]]
     grouped.OMdata_swapped[[x]] <- OMdata_swapped #save optomotor data to groupwise list
     rm(OMdata_swapped) #remove the optomotor data frame so it can be generated again for the next group
