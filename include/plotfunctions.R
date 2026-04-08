7d4ec93d0434564b76af68bc3769b8cb635e1c43
lunicorn36 <ipeksubay36@gmail.com>
2026-04-08 09:25:34 +0200

    Update plotfunctions.R

--- include/plotfunctions.R
+++ include/plotfunctions.R
@@ -126,32 +126,14 @@ geom_split_violin <- function(mapping = NULL, data = NULL, stat = "ydensity", po
 
 ############### generate hyperlinks to flybase IDs
 
-hyperlinks.FBidsmale <- function(FBidsmale){
+hyperlinks.FBids <- function(FBids){
   #generate dataframe with hyperlinked FBids where each group gets one row
-<<<<<<< Updated upstream
-    id.frame<-as.data.frame(lapply(read.csv(text = FBidsmale, header = FALSE, na.strings=c("","NA")), function(x) ifelse(!is.na(x), paste('<a href="http://flybase.org/reports/',x,'">',x,'</a>', sep = ''),NA)))
-    id.frame[is.na(id.frame)]<-""                     #remove NAs
-    FBidsmale=apply(id.frame,1,paste,collapse=",")        #create strings
-    FBidsmale=gsub("^,*|(?<=,),|,*$", "", FBidsmale, perl=T)  #remove trailing/leading commas
-    FBidsmale=gsub('<a href="http://flybase.org/reports/none">none</a>', "none", FBidsmale) #remove the link from 'none' FBidsmale
-  return(FBidsmale)
-}
-hyperlinks.FBidsfemale <- function(FBidsfemale){
-  #generate dataframe with hyperlinked FBids where each group gets one row
-  id.frame<-as.data.frame(lapply(read.csv(text = FBidsfemale, header = FALSE, na.strings=c("","NA")), function(x) ifelse(!is.na(x), paste('<a href="http://flybase.org/reports/',x,'">',x,'</a>', sep = ''),NA)))
-  id.frame[is.na(id.frame)]<-""                     #remove NAs
-  FBidsfemale=apply(id.frame,1,paste,collapse=",")        #create strings
-  FBidsfemale=gsub("^,*|(?<=,),|,*$", "", FBidsfemale, perl=T)  #remove trailing/leading commas
-  FBidsfemale=gsub('<a href="http://flybase.org/reports/none">none</a>', "none", FBidsfemale) #remove the link from 'none' FBidsfemale
-  return(FBidsfemale)
-=======
   id.frame<-as.data.frame(lapply(read.csv(text = FBids, header = FALSE, na.strings=c("","NA")), function(x) ifelse(!is.na(x), paste('<a href="http://flybase.org/reports/',x,'">',x,'</a>', sep = ''),NA)))
   id.frame[is.na(id.frame)]<-""                     #remove NAs
   FBids=apply(id.frame,1,paste,collapse=",")        #create strings
   FBids=gsub("^,*|(?<=,),|,*$", "", FBids, perl=T)  #remove trailing/leading commas
   FBids=gsub('<a href="http://flybase.org/reports/none">none</a>', "none", FBids) #remove the link from 'none' FBids
   return(FBids)
->>>>>>> Stashed changes
 }
 
 ###########################################################################
